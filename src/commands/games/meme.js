22c28965b33a41468d29a2bc87e489743d3fba8b
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-23 16:13:36 +0200

    :frog: Added meme command

---
+++ src/commands/games/meme.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+    name: "meme",
+    description: "Returns a meme",
+    async execute(bot, message) {
+        const data = await fetch("https://meme-api.herokuapp.com/gimme").then(res => res.json());
+
+        const embed = new MessageEmbed()
+            .setFooter(message.author.username)
+            .setTitle(data.title)
+            .setColor("BLUE")
+            .setDescription(`[Click here if the image failed to load.](${data.url})`)
+            .setImage(`${data.url}`)
+            .setTimestamp();
+
+        message.channel.send(embed);
+    }
+};
