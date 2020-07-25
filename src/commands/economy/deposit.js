34188b3d74bce4f588de565a6a0b1f78444a6712
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-25 13:04:20 +0200

    :bug: Fixed deposit command

--- src/commands/economy/deposit.js
+++ src/commands/economy/deposit.js
@@ -4,15 +4,13 @@ module.exports = {
     name: "deposit",
     description: "deposit money to your bank",
     category: "economy",
+    usage: "!deposit <all | amount>",
     async execute(bot, message, args) {
         const user = message.author;
-        const amount = Number(args[0]);
+        let amount = args[0];
 
         if (!amount)
             return message.reply("Please provide an amount to deposit");
-
-        if (typeof amount !== "number")
-            return message.reply("Please provide a valid amount to deposit");
 
 
         const money = db.fetch(`money_${message.guild.id}_${user.id}`);
@@ -23,6 +21,13 @@ module.exports = {
             db.subtract(`money_${message.guild.id}_${user.id}`, money);
             return message.channel.send("Successfully deposited all your money!");
         }
+
+
+        amount = Number(args[0]);
+
+        if (typeof amount !== "number" || isNaN(amount))
+            return message.reply("Please provide a valid amount to deposit");
+
 
         if (money <= amount)
             return message.channel.send("You don't have that much money!");
