c4484d3b4cdaed089336d838961c1ab4e3220f17
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-08-06 15:48:18 +0200

    :tada: Added nickname command(botowner)

---
+++ src/commands/botowner/nickname.js
@@ -0,0 +1,17 @@
+const { ownerId } = require("../../../config.json");
+
+module.exports = {
+  name: "nickname",
+  description: "Set the bot's nick name in a guild",
+  category: "botowner",
+  async execute(bot, message, args) {
+    if (message.author.id !== ownerId)
+      return message.reply("Only the owner is allowed to run this command");
+
+    const nickname = args.join(" ");
+
+    message.guild.members.cache
+      .get(bot.user.id)
+      .setNickname(nickname, "Updated by bot-owner");
+  },
+};
