f8056aeb1c637f014dee0aa426225f0ee0bf7fdc
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-09 19:55:42 +0200

    :tada: Updated stealemoji

--- src/commands/admin/stealemoji.js
+++ src/commands/admin/stealemoji.js
@@ -1,19 +1,27 @@
 const Discord = require("discord.js");
 const { parse } = require("twemoji-parser");
 const { MessageEmbed } = require("discord.js");
-const Color = `RANDOM`;
 
 module.exports = {
   name: "stealemoji",
   category: "admin",
+  usage: "stealemoji <emoji> <custom name>",
+  description: "Steal an emoji from a different server",
   async execute(bot, message, args) {
-    
-    if (!message.member.hasPermission(`MANAGE_EMOJIS`)) {
-      return message.channel.send(`You Don't Have Permission To Use This Command! Manage Emojis`)
+    if (!message.member.hasPermission("MANAGE_EMOJIS")) {
+      return message.channel.send(
+        "You Don't Have Permission To Use This Command! (Manage Emojis)"
+      );
     }
-    
+
+    if (!message.guild.me.hasPermission("MANAGE_EMOJIS")) {
+      return message.channel.send(
+        "I don't have permissions to create emojis for this server (Manage Emojis)"
+      );
+    }
+
     const emoji = args[0];
-    if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
+    if (!emoji) return message.channel.send("Please Give Me A Emoji!");
 
     let customemoji = Discord.Util.parseEmoji(emoji);
 
@@ -22,24 +30,27 @@ module.exports = {
         customemoji.animated ? "gif" : "png"
       }`;
       const name = args.slice(1).join(" ");
+
       message.guild.emojis.create(
         `${Link}`,
         `${name || `${customemoji.name}`}`
       );
       const Added = new MessageEmbed()
-        .setTitle(`Emoji Added`)
-        .setColor(`${Color}`)
+        .setTitle("Emoji Added")
+        .setColor("BLUE")
         .setDescription(
-          `Emoji Has Been Added! | Name : ${name || `${customemoji.name}`} | Preview : [Click Me](${Link})`
+          `Emoji Has Been Added! | Name : ${
+            name || `${customemoji.name}`
+          } | Preview : [Click Me](${Link})`
         );
       return message.channel.send(Added);
     } else {
       let CheckEmoji = parse(emoji, { assetType: "png" });
       if (!CheckEmoji[0])
-        return message.channel.send(`Please Give Me A Valid Emoji!`);
+        return message.channel.send("Please Give Me A Valid Emoji!");
       message.channel.send(
-        `You Can Use Normal Emoji Without Adding In Server!`
+        "You Can Use Normal Emoji Without Adding In Server!"
       );
     }
-  }
+  },
 };
