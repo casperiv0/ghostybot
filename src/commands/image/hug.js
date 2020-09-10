09f592a4f3d5d1c908bb6d0c2d5461c41d88634d
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-10 16:14:45 +0200

    :tada: Updated image commands

--- src/commands/image/hug.js
+++ src/commands/image/hug.js
@@ -2,19 +2,24 @@ const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
 
 module.exports = {
-    name: "hug",
-    description: "Shows a picture of people hugging",
-    category: "image",
-    async execute(bot, message) {
-        const data = await fetch("https://nekos.life/api/hug").then(res => res.json());
+  name: "hug",
+  description: "Shows a picture of people hugging",
+  category: "image",
+  async execute(bot, message) {
+    const data = await fetch("https://nekos.life/api/hug").then((res) =>
+      res.json()
+    );
+    const user = message.mentions.users.first() || message.author;
+    const hugged = message.author.id === user.id ? "themselfs" : user.username;
 
-        const embed = new MessageEmbed()
-            .setFooter(message.author.username)
-            .setColor("BLUE")
-            .setDescription(`[Click here if the image failed to load.](${data.url})`)
-            .setImage(`${data.url}`)
-            .setTimestamp();
+    const embed = new MessageEmbed()
+      .setTitle(`${message.author.username} Hugged ${hugged}`)
+      .setFooter(message.author.username)
+      .setColor("BLUE")
+      .setDescription(`[Click here if the image failed to load.](${data.url})`)
+      .setImage(`${data.url}`)
+      .setTimestamp();
 
-        message.channel.send(embed);
-    }
+    message.channel.send(embed);
+  },
 };
