b1f0c934209e388b8c153f220c73ec5250cdf311
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-10 20:13:11 +0200

    :tada: format code & add check for feedBackChannelId

--- src/commands/levels/resetxp.js
+++ src/commands/levels/resetxp.js
@@ -1,21 +1,20 @@
-const { setUserXp } = require('../../utils/functions')
+const { setUserXp } = require("../../utils/functions");
 
 module.exports = {
-    name: "resetxp", // name can't have a space, use args instead
-    description: "reset all users xp for current server",
-    category: "levels",
-    usage: "resetxp all",
-    async execute(bot, message, args) {
-        if(!message.member.hasPermission("MANAGE_GUILD"))
-        return message.channel.send("You need Manage Guild permission")
-        const users = await message.guild.members.fetch();
+  name: "resetxp", // name can't have a space, use args instead
+  description: "reset all users xp for current server",
+  category: "levels",
+  usage: "resetxp",
+  async execute(bot, message) {
+    if (!message.member.hasPermission("MANAGE_GUILD"))
+      return message.channel.send("You need Manage Guild permission");
+    const users = await message.guild.members.fetch();
 
-        users.forEach(user => {
-            
-            setUserXp(message.guild.id, user.id, 0)
-        })
+    users.forEach((user) => {
+      setUserXp(message.guild.id, user.id, 0);
+    });
 
-        // send message
-        message.channel.send("Succesfully reseted everyone's xp")
-    }
-}
+    // send message
+    message.channel.send("Succesfully reseted everyone's xp");
+  },
+};
