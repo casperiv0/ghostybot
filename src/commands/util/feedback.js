37d06571694727cb724eb72f3721643994ca2ea1
tovade <66245404+tovade@users.noreply.github.com>
2020-09-10 20:05:40 +0200

    Create feedback.js

---
+++ src/commands/util/feedback.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+const { feedBackChannelId } = require("../../../config.json");
+
+module.exports = {
+    name: "feedback",
+    description: "Give feedback about the bot",
+    category: "util",
+    execute(bot, message, args) {
+        const feedback = args.join(" ");
+
+        if (!feedback) return message.channel.send("If u wanna be nice please give some feedback.");
+
+        const embed = new MessageEmbed()
+            .setColor("BLUE")
+            .setTitle(`${message.author.username} New Feedback`)
+            .setDescription(feedback)
+            .setFooter(message.author.username)
+            .setTimestamp();
+
+        bot.channels.cache.get(feedBackChannelId).send(embed);
+
+    }
+};
