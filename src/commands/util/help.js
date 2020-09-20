7afccedd816c3b3e7445dc48aad765769d1054d6
tovade <66245404+tovade@users.noreply.github.com>
2020-09-20 20:10:48 +0200

    Update help.js

--- src/commands/util/help.js
+++ src/commands/util/help.js
@@ -83,51 +83,48 @@ module.exports = {
       .filter(({ category }) => category === "image")
       .map(({ name }) => name)
       .join(", ");
+      const hentaiCmds = commands
+      .filter(({ category }) => category === "hentainsfw")
+      .map(({ name }) => name)
+      .join(", ");
 
+    const embed = new MessageEmbed() 
+      .setTimestamp()
+      .setFooter(message.author.username)
+      .setColor("BLUE")
+      .addField("Admin Commands", `\`\`\`${adminCmds}\`\`\``)
+      .addField("Animal Commands", `\`\`\`${animalCmds}\`\`\``);
+    if (ownerId === message.author.id) {
+      embed.addField("BotOwner Commands", `\`\`\`${botOwnerCmds}\`\`\``);
+    }
+    if (ownerId !== message.author.id) {
+      embed.addField(
+        "BotOwner Commands",
+        "only the owner is allowed to see this!"
+      );
+    }
+    if (nsfw) {
+      embed.addField("NSFW Commands", `\`\`\`${nsfwCmds}\`\`\``)
+      embed.addField("NSFW Commands", `\`\`\`${hentaiCmds}\`\`\``)
+    } else {
+      embed.addField(
+        "NSFW Commands",
+        "To view nfsw commands check in an nfsw channel!"
+      );
+    }
+    embed
+      .addField("Game Commands", `\`\`\`${gameCmds}\`\`\``)
+      .addField("Image Commands", `\`\`\`${imageCmds}\`\`\``)
+      .addField("Music Commands", `\`\`\`${musicCmds}\`\`\``)
+      .addField("Util Commands", `\`\`\`${utilsCmds}\`\`\``)
+      .addField("Economy Commands", `\`\`\`${economyCmds}\`\`\``)
+      .addField("Levels Commands", `\`\`\`${levelCmds}\`\`\``)
+      .addField("Server prefix: ", prefix)
+      .setDescription(
+        `use \`${prefix}help <command name | alias>\` to view more info about a command\n More info can be found using the \`botinfo\` command`
+      )
+      .setTitle("Help");
 
-            return message.channel.send(embed);
-        }
-        let nsfw = message.channel.nsfw;
-        const commands = bot.commands;
-        const utilsCmds = commands.filter(({category}) => category === "util").map(({name}) => name).join(", ");
-        const adminCmds = commands.filter(({category}) => category === "admin").map(({name}) => name).join(", ");
-        const animalCmds = commands.filter(({category}) => category === "animal").map(({name}) => name).join(", ");
-        const botOwnerCmds = commands.filter(({category}) => category === "botowner").map(({name}) => name).join(", ");
-        const gameCmds = commands.filter(({category}) => category === "games").map(({name}) => name).join(", ");
-        const musicCmds = commands.filter(({category}) => category === "music").map(({name}) => name).join(", ");
-        const nsfwCmds = commands.filter(({category}) => category === "nsfw").map(({name}) => name).join(", ");
-        const economyCmds = commands.filter(({category}) => category === "economy").map(({name}) => name).join(", ");
-        const levelCmds = commands.filter(({category}) => category=== "levels").map(({name}) => name).join(", ");
-        const imageCmds = commands.filter(({category}) => category=== "image").map(({name}) => name).join(", ");
-        const hentaiCmds = commands.filter(({category}) => category === "hentainsfw").map(({name}) => name).join(", ");
-
-        const embed = new MessageEmbed()
-            .setTimestamp()
-            .setFooter(message.author.username)
-            .setColor("BLUE")
-            .addField("Admin Commands", `\`\`\`${adminCmds}\`\`\``)
-            .addField("Animal Commands", `\`\`\`${animalCmds}\`\`\``)
-            if(ownerId === message.author.id) {
-                embed.addField("BotOwner Commands", `\`\`\`${botOwnerCmds}\`\`\``)
-            }
-            if(ownerId !== message.author.id) {
-                embed.addField("BotOwner Commands", `only the owner is allowed to see this!`)
-            }
-            embed.addField("Game Commands", `\`\`\`${gameCmds}\`\`\``)
-            .addField("Image Commands", `\`\`\`${imageCmds}\`\`\``)
-            .addField("Music Commands", `\`\`\`${musicCmds}\`\`\``)
-            .addField("Util Commands", `\`\`\`${utilsCmds}\`\`\``)
-            .addField("Economy Commands", `\`\`\`${economyCmds}\`\`\``)
-            .addField("Levels Commands", `\`\`\`${levelCmds}\`\`\``)
-         if(nsfw) {
-            embed.addField("NSFW Commands", `\`\`\`${nsfwCmds}\`\`\``)
-            embed.addField("Hentai NSFW Commands", `\`\`\`${hentaiCmds}\`\`\``)
-             }
-              if(!nsfw) embed.addField("NSFW Commands", `To view nfsw commands check in an nfsw channel!`)
-              .addField("Server prefix: ", prefix)
-              .setDescription(`use \`${prefix}help <command name | alias>\` to view more info about a command\n More info can be found using the \`botinfo\` command`)
-              .setTitle("Help");
-
-        message.channel.send(embed);
-        }
+    message.channel.send(embed);
+  },
 };
