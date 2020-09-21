09949043d915052384473d3ae6fbbeec80345743
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-21 17:54:50 +0200

    :tada: Format code

--- src/commands/NSFW/anal.js
+++ src/commands/NSFW/anal.js
@@ -1,23 +1,25 @@
-const { MessageEmbed } = require('discord.js');
-const fetch = require('node-fetch');
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = {
-name: "anal",
-description: "anal nsfw",
-category: "nsfw",
-async execute(bot, message, args) {
+  name: "anal",
+  description: "anal nsfw",
+  category: "nsfw",
+  async execute(bot, message) {
     if (!message.channel.nsfw)
-    return message.channel.send("This channel is not a NSFW channel!");
-    const data = await fetch("https://nekobot.xyz/api/image?type=anal").then((res) => res.json());
+      return message.channel.send("This channel is not a NSFW channel!");
+    const data = await fetch(
+      "https://nekobot.xyz/api/image?type=anal"
+    ).then((res) => res.json());
     const embed = new MessageEmbed()
-    .setFooter(message.author.username)
-    .setColor("BLUE")
-    .setDescription(
-      `[Click here if the image failed to load.](${data.message})`
-    )
-    .setImage(`${data.message}`)
-    .setTimestamp();
+      .setFooter(message.author.username)
+      .setColor("BLUE")
+      .setDescription(
+        `[Click here if the image failed to load.](${data.message})`
+      )
+      .setImage(`${data.message}`)
+      .setTimestamp();
 
-  message.channel.send(embed);
-}
+    message.channel.send(embed);
+  },
 };
--- src/commands/NSFW/gonewild.js
+++ src/commands/NSFW/gonewild.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const fetch = require('node-fetch')
-
+const fetch = require("node-fetch");
 
 module.exports = {
   name: "gonewild",
@@ -8,18 +7,19 @@ module.exports = {
   category: "nsfw",
   async execute(bot, message) {
     if (!message.channel.nsfw)
-    return message.channel.send("This channel is not a NSFW channel!");
-    const data = await fetch("https://nekobot.xyz/api/image?type=gonewild").then((res) => res.json());
+      return message.channel.send("This channel is not a NSFW channel!");
+    const data = await fetch(
+      "https://nekobot.xyz/api/image?type=gonewild"
+    ).then((res) => res.json());
     const embed = new MessageEmbed()
-    .setFooter(message.author.username)
-    .setColor("BLUE")
-    .setDescription(
-      `[Click here if the image failed to load.](${data.message})`
-    )
-    .setImage(`${data.message}`)
-    .setTimestamp();
+      .setFooter(message.author.username)
+      .setColor("BLUE")
+      .setDescription(
+        `[Click here if the image failed to load.](${data.message})`
+      )
+      .setImage(`${data.message}`)
+      .setTimestamp();
 
-  message.channel.send(embed);
- 
-  }
-}
+    message.channel.send(embed);
+  },
+};
