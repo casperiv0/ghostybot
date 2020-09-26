1e00ce73fec7c2e7f5360774024e3fa6b2b84041
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-26 13:05:34 +0200

    :sparkles: Added limit to volume command

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -1,19 +1,23 @@
 module.exports = {
-    name: "volume",
-    description: "Set the volume between 1 to 100",
-    category: "music",
-    execute(bot, message, args, serverQueue) {
-        if (!message.member.voice.channel) {
-            return message.channel.send("You need to be in a voice channel!");
-        }
-
-        if (!serverQueue) {
-            return message.channel.send("There are no songs currently playing");
-        }
-
-        const volume = args[0] / 100;
-        serverQueue.volume = volume;
-        serverQueue.connection.dispatcher.setVolume(volume);
-        message.channel.send(`Successfully set volume to ${args[0]}%`);
+  name: "volume",
+  description: "Set the volume between 1 to 100",
+  category: "music",
+  execute(bot, message, args, serverQueue) {
+    if (!message.member.voice.channel) {
+      return message.channel.send("You need to be in a voice channel!");
     }
+
+    if (!serverQueue) {
+      return message.channel.send("There are no songs currently playing");
+    }
+
+    if (Number(args[0]) > 100) {
+      return message.channel.send("Volume must be between 0 and 100");
+    }
+
+    const volume = args[0] / 100;
+    serverQueue.volume = volume;
+    serverQueue.connection.dispatcher.setVolume(volume);
+    message.channel.send(`Successfully set volume to ${args[0]}%`);
+  },
 };
