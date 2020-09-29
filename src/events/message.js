799ae58b1d9f9f0d3bd1bcd68feef7a231d7e3b0
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-09-29 16:10:21 +0200

    :sparkles: Update code & add better avatar quality

--- src/events/message.js
+++ src/events/message.js
@@ -10,7 +10,7 @@ const {
 } = require("../utils/functions");
 const db = require("quick.db");
 const queue = new Map();
-const ownerId = require("../../config.json")
+const ownerId = require("../../config.json");
 module.exports = {
   name: "message",
   async execute(bot, message) {
@@ -19,19 +19,6 @@ module.exports = {
     const guildId = message.guild.id;
     const userId = message.author.id;
     const cooldowns = bot.cooldowns;
-
-    const blacklistedUsers = getBlacklistUsers();
-
-    if (blacklistedUsers !== null) {
-      const isBlacklisted = getBlacklistUsers().filter(
-        (u) => u.id === message.author.id
-      )[0];
-
-      if (isBlacklisted) {
-        return message.reply("You've been blacklisted from using this bot.");
-      }
-    }
-  
     const blacklistedUsers = await getBlacklistUsers();
 
     const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
@@ -77,9 +64,6 @@ module.exports = {
     const [, matchedPrefix] = message.content.match(prefix);
     const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
-    if(command.ownerOnly && message.author.id !== ownerId) {
-      return message.reply("Only for owners!")
-    }
     let customCmds = db.get(`cmds_${message.guild.id}`);
 
     if (blacklistedUsers !== null) {
@@ -108,6 +92,10 @@ module.exports = {
         const now = Date.now();
         const timestamps = cooldowns.get(cmd.name);
         const cooldownAmount = cmd.cooldown * 1000;
+
+        if (cmd.ownerOnly && message.author.id !== ownerId) {
+          return message.reply("This command can only be used by the owner!");
+        }
 
         if (timestamps.has(userId)) {
           const expTime = timestamps.get(userId) + cooldownAmount;
