2e7853be8bd1c2feeced77fac28bb9dfa9401e32
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-03 08:12:16 +0200

    :bug: fixed bug on feedback & bugreport command

--- src/commands/util/bugreport.js
+++ src/commands/util/bugreport.js
@@ -2,22 +2,27 @@ const { MessageEmbed } = require("discord.js");
 const { reportsChannelId } = require("../../../config.json");
 
 module.exports = {
-    name: "bugreport",
-    description: "Report a bug to your staff",
-    category: "util",
-    execute(bot, message, args) {
-        const bug = args.join(" ");
+  name: "bugreport",
+  description: "Report a bug to your staff",
+  category: "util",
+  execute(bot, message, args) {
+    const bug = args.join(" ");
 
-        if (!bug) return message.channel.send("Please provide a bug");
+    if (!bug) return message.channel.send("Please provide a bug");
 
-        const embed = new MessageEmbed()
-            .setColor("BLUE")
-            .setTitle(`${message.author.username} has reported a bug`)
-            .setDescription(bug)
-            .setFooter(message.author.username)
-            .setTimestamp();
+    const embed = new MessageEmbed()
+      .setColor("BLUE")
+      .setTitle(`${message.author.username} has reported a bug`)
+      .setDescription(bug)
+      .setFooter(message.author.username)
+      .setTimestamp();
 
-        bot.channels.cache.get(reportsChannelId).send(embed);
-
+    if (!reportsChannelId) {
+      return message.channel.send(
+        "reportsChannelId needs to be provided, for this command to work."
+      );
     }
+
+    bot.channels.cache.get(reportsChannelId).send(embed);
+  },
 };
