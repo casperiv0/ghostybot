6cf39887526427d2a122711cfefc7f0f93163929
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-10 18:30:38 +0200

    format code

--- src/commands/util/hastebin.js
+++ src/commands/util/hastebin.js
@@ -5,20 +5,25 @@ module.exports = {
   aliases: ["haste"],
   description: "Get a link of hastebin for your text",
   category: "util",
+  usage: "hastbin <extension (js, ts, ...)> <code>",
   async execute(bot, message, args) {
-    const extaintion = args[0];
+    const extension = args[0];
     const code = args.slice(1).join(" ");
 
     if (!args.length) {
-      return message.channel.send("Please write your text or code to generate hastebin link");
+      return message.channel.send(
+        "Please write your text or code to generate hastebin link"
+      );
     }
 
     try {
-      const haste = await hastebin(`${code}`, { extension: `${extaintion}` });
+      const haste = await hastebin(`${code}`, { extension: `${extension}` });
 
       message.channel.send(haste);
     } catch (e) {
-      return message.channel.send("Something went wrong, Please try again later.");
+      return message.channel.send(
+        "Something went wrong, Please try again later."
+      );
     }
   },
 };
