7c7602921f7d67d55705b4483933929f38af5bb6
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-10 08:18:26 +0200

    :tada: updated playstore command

--- src/commands/util/playstore.js
+++ src/commands/util/playstore.js
@@ -8,23 +8,20 @@ module.exports = {
   usage: "playstore <Application Name>",
   category: "util",
   async execute(bot, message, args) {
-    if (!args[0])
-      return message.channel.send(
-        `Please Give Something To Search - ${message.author.username}`
-      );
+    const search = args.join(" ");
+
+    if (!search) return message.channel.send("Please provide search query");
 
     PlayStore.search({
       term: args.join(" "),
-      num: 1
-    }).then(Data => {
+      num: 1,
+    }).then((Data) => {
       let App;
 
       try {
         App = JSON.parse(JSON.stringify(Data[0]));
       } catch (error) {
-        return message.channel.send(
-          "No Application Found !"
-        );
+        return message.channel.send("No Application Found!");
       }
 
       const Embed = new MessageEmbed()
@@ -33,13 +30,13 @@ module.exports = {
         .setURL(App.url)
         .setTitle(`${App.title}`)
         .setDescription(App.summary)
-        .addField(`Price`, App.priceText, true)
-        .addField(`Developer`, App.developer, true)
-        .addField(`Score`, App.scoreText, true)
+        .addField("Price", App.priceText, true)
+        .addField("Developer", App.developer, true)
+        .addField("Score", App.scoreText, true)
         .setFooter(`Requested by ${message.author.username}`)
         .setTimestamp();
 
       return message.channel.send(Embed);
     });
-  }
+  },
 };
