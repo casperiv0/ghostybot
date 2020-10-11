583efa35bd3b6b28b211f34ebfc6179fb048fc50
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-11 08:32:13 +0200

    :hammer_and_pick: updated pokemon.js

--- src/commands/util/pokemon.js
+++ src/commands/util/pokemon.js
@@ -6,35 +6,54 @@ module.exports = {
   description: "Returns a pokemon information",
   category: "util",
   async execute(bot, message, args) {
-    const query = args.join("%20");
+    const query = args.join(" ");
 
-    if (!query) return message.channel.send("Please provide a pokemon name !");
+    if (!query) return message.channel.send("Please provide a pokemon name!");
 
     try {
-      const data = await fetch(`https://some-random-api.ml/pokedex?pokemon=${query}`).then(res => res.json());
+      const data = await fetch(
+        `https://some-random-api.ml/pokedex?pokemon=${encodeURIComponent(
+          query
+        )}`
+      ).then((res) => res.json());
 
       const embed = new MessageEmbed()
         .setFooter(message.author.username)
         .setTitle(data.name)
-        .setColor("RANDOM")
+        .setColor("BLUE")
         .setDescription(data.description)
-        .addField("ID", data.id)
-        .addField("Type", data.type)
-        .addField("Species", data.species)
-        .addField("Abilities", data.abilities)
-        .addField("Height", data.height)
-        .addField("Weight", data.weight)
-        .addField("Experience", data.base_experience)
-        .addField("Gender", data.gender)
-        .addField("Egg Groups", data.egg_groups)
-        .addField("**Family :-**", `**Evolution Stage :** ${data.family.evolutionStage} \n**Evolution Line :** ${data.family.evolutionLine} `)
-        .addField("**Stats :-**", `**HP :** ${data.stats.hp} \n**Attack :** ${data.stats.attack} \n**Defense: ** ${data.stats.defense} \n**SP ATK :** ${data.stats.sp_atk} \n**SP DEF :** ${data.stats.sp_def} \n**Speed :** ${data.stats.speed} \n**Total :** ${data.stats.total} `)
+        .addField("ID", data.id, true)
+        .addField("Type", data.type, true)
+        .addField("Species", data.species, true)
+        .addField("Abilities", data.abilities, true)
+        .addField("Height", data.height, true)
+        .addField("Weight", data.weight, true)
+        .addField("Experience", data.base_experience, true)
+        .addField("Gender", data.gender, true)
+        .addField("Egg Groups", data.egg_groups, true)
+        .addField(
+          "**Family:**",
+          `**Evolution Stage:** ${data.family.evolutionStage} 
+          **Evolution Line:** ${data.family.evolutionLine}`
+        )
+        .addField(
+          "**Stats:**",
+          `**HP:** ${data.stats.hp} 
+          **Attack:** ${data.stats.attack} 
+          **Defense:** ${data.stats.defense} 
+          **SP ATK:** ${data.stats.sp_atk} 
+          **SP DEF:** ${data.stats.sp_def} 
+          **Speed:** ${data.stats.speed} 
+          **Total:** ${data.stats.total}`
+        )
         .setThumbnail(`${data.sprites.animated}`)
         .setTimestamp();
 
       message.channel.send({ embed });
     } catch (e) {
-      return message.channel.send(`No pokemon was found with ${query}. Please use correct spelling and try again later.`);
+      return message.channel.send(
+        `No pokemon was found with ${query}. Please use correct spelling and try again later.`
+      );
     }
   },
 };
