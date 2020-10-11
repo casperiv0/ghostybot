374b9429d51fa745459854d7b87e0c29f10ffca6
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-11 08:36:21 +0200

    :hammer_and_pick: updated skin.js

--- src/commands/util/skin.js
+++ src/commands/util/skin.js
@@ -1,28 +1,36 @@
 const { MessageEmbed } = require("discord.js");
-const MojangAPI = require('mojang-api');
+const MojangAPI = require("mojang-api");
 
 module.exports = {
   name: "skin",
   description: "Search for skins from Minecraft",
   category: "info",
-  async execute(message, args) {
-  if !args) return message.channel.send('Please write the name of the skin')
+  aliases: ["minecraftskin"],
+  async execute(bot, message, args) {
+    const search = args.join(" ");
 
-    MojangAPI.nameToUuid(args, (err, res) => {
-    if (err)
-        console.log(err);
-    else {
-        const uuid = res[0]?.id
-        if(!uuid) return message.channel.send(`Player \`${args}\` not found!`)
-        const full = `https://visage.surgeplay.com/full/2048/${uuid}.png`
-        const skin = `https://visage.surgeplay.com/skin/2048/${uuid}.png`
-        const face = `https://visage.surgeplay.com/face/2048/${uuid}.png`
-        message.channel.send(new MessageEmbed()
-        .setAuthor(`Player skin ${res[0].name}`, face)
-        .setDescription(`[Download skin](${skin})`)
-        .setImage(full)
-        .setColor("303136"));
+    if (!search) {
+      return message.channel.send("Please write the name of the skin");
     }
+
+    MojangAPI.nameToUuid(search, (err, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const uuid = res[0]?.id;
+        if (!uuid)
+          return message.channel.send(`Player \`${search}\` not found!`);
+        const full = `https://visage.surgeplay.com/full/2048/${uuid}.png`;
+        const skin = `https://visage.surgeplay.com/skin/2048/${uuid}.png`;
+        const face = `https://visage.surgeplay.com/face/2048/${uuid}.png`;
+        message.channel.send(
+          new MessageEmbed()
+            .setAuthor(`Player skin ${res[0].name}`, face)
+            .setDescription(`[Download skin](${skin})`)
+            .setImage(full)
+            .setColor("BLUE")
+        );
+      }
     });
   },
 };
