c187b70e60165fee9f5ae0c5ba3a87c1f1fbfa08
iamlakhindar <72681474+iamlakhindar@users.noreply.github.com>
2020-10-11 15:01:09 +0530

    Create amazingearth.js

---
+++ src/commands/image/amazingearth.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "earthporn",
+  aliases: ["amazingearth"],
+  description: "Amazing images of light and landscape",
+  category: "image",
+  async execute(bot, message) {
+    const data = await fetch(
+      "https://www.reddit.com/r/Earthporn/random/.json"
+    ).then((res) => res.json());
+
+    const children = data[0].data.children[0];
+    const permaLink = children.data.permalink;
+    const url = `https://reddit.com${permaLink}`;
+    const image = children.data.url;
+    const title = children.data.title;
+    const upvotes = children.data.ups;
+    const comments = children.data.num_comments;
+
+    const embed = new MessageEmbed()
+      .setColor("BLUE")
+      .setTitle(`${title}`)
+      .setURL(url)
+      .setImage(image)
+      .setFooter(`👍 ${upvotes} 💬 ${comments}`);
+
+    message.channel.send({ embed });
+  },
+};
