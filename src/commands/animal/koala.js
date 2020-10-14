f4a71f3ffdf95794a865356a192808c33e0788d3
iamlakhindar <72681474+iamlakhindar@users.noreply.github.com>
2020-10-14 21:20:47 +0530

    Create  koala.js

---
+++ src/commands/animal/koala.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "koala",
+  description: "Shows a random picture of koala",
+  category: "animal",
+  async execute(bot, message) {
+    const data = await fetch("https://some-random-api.ml/img/koala").then((res) =>
+      res.json()
+    );
+
+    const embed = new MessageEmbed()
+      .setFooter(message.author.username)
+      .setColor("BLUE")
+      .setDescription(
+        `[Click here if the image failed to load.](${data.link})`
+      )
+      .setImage(`${data.link}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  },
+};
