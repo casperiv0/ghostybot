25d0435813db5e4c3a92388ca61c7093d77ec878
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-15 16:13:15 +0200

    ⚒ format code

--- src/commands/animal/bear.js
+++ src/commands/animal/bear.js
@@ -6,14 +6,14 @@ module.exports = {
   description: "Shows a random picture of bear and fact",
   category: "animal",
   async execute(bot, message) {
-    const data = await fetch("https://no-api-key.com/api/v1/animals/bear").then((res) =>
-      res.json()
-    );
+    const data = await fetch(
+      "https://no-api-key.com/api/v1/animals/bear"
+    ).then((res) => res.json());
 
     const embed = new MessageEmbed()
       .setFooter(message.author.username)
       .setColor("BLUE")
-     .setTitle(`${data.fact}`)
+      .setTitle(`${data.fact}`)
       .setDescription(
         `[Click here if the image failed to load.](${data.image})`
       )
--- src/commands/animal/koala.js
+++ src/commands/animal/koala.js
@@ -6,16 +6,14 @@ module.exports = {
   description: "Shows a random picture of koala",
   category: "animal",
   async execute(bot, message) {
-    const data = await fetch("https://some-random-api.ml/img/koala").then((res) =>
-      res.json()
-    );
+    const data = await fetch(
+      "https://some-random-api.ml/img/koala"
+    ).then((res) => res.json());
 
     const embed = new MessageEmbed()
       .setFooter(message.author.username)
       .setColor("BLUE")
-      .setDescription(
-        `[Click here if the image failed to load.](${data.link})`
-      )
+      .setDescription(`[Click here if the image failed to load.](${data.link})`)
       .setImage(`${data.link}`)
       .setTimestamp();
 
