a53d8fb1729818a9eb4adefe550d9051ffae5af5
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-16 19:14:33 +0200

    :tada: Updated play & nowplaying command

--- src/commands/music/nowplaying.js
+++ src/commands/music/nowplaying.js
@@ -1,25 +1,35 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "nowplaying",
-    description: "Shows info about the current playing song",
-    category: "music",
-    aliases: ["np", "now"],
-    execute(bot, message, args, serverQueue) {
-        if (!message.member.voice.channel) {
-            return message.channel.send("You need to be in a voice channel!");
-        }
-
-        if (!serverQueue) {
-            return message.channel.send("There are no songs currently playing");
-        }
-
-        const embed = new MessageEmbed()
-            .setTitle("Now Playing")
-            .setColor("BLUE")
-            .setDescription(`${serverQueue.nowPlaying.title}`
-            )
-            .setImage(serverQueue.nowPlaying.thumbnail);
-        message.channel.send(embed);
+  name: "nowplaying",
+  description: "Shows info about the current playing song",
+  category: "music",
+  aliases: ["np", "now"],
+  execute(bot, message, args, serverQueue) {
+    if (!message.member.voice.channel) {
+      return message.channel.send("You need to be in a voice channel!");
     }
+
+    if (!serverQueue) {
+      return message.channel.send("There are no song currently playing");
+    }
+
+    const song = serverQueue.nowPlaying;
+
+    const embed = new MessageEmbed()
+      .setTitle(song.title)
+      .setURL(song.url)
+      .setAuthor("🎵 Now playing:")
+      .setImage(song.thumbnail)
+      .setColor("BLUE")
+      .setDescription(
+        `
+      **Duration:** ${song.duration}s
+      **Uploaded by:** ${song.uploadedBy}
+      **Uploaded at:** ${song.uploadedAt}
+      **Likes:** ${song.likes}`
+      )
+      .setFooter(`Requested by ${song.requestedBy.username}`);
+    message.channel.send(embed);
+  },
 };
