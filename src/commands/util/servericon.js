2a94504bc9dec6d659ba9d18f64ec80b77c0069c
Xaliks <64866197+Xaliks@users.noreply.github.com>
2020-10-19 18:42:01 +0300

    Update servericon.js
    if there is no icon on the server, then a message is sent about the absence of the icon on the server, otherwise the bot will send this: https://i.imgur.com/OZrjzF8.png

--- src/commands/util/servericon.js
+++ src/commands/util/servericon.js
@@ -5,8 +5,11 @@ module.exports = {
   description: "Shows the server icon",
   category: "util",
   execute(bot, message) {
-    const icon = message.guild.iconURL({ dynamic: true, size: 1024 });
-
+    const icon = message.guild.iconURL({ dynamic: true, size: 2048 });
+if (icon === null) {
+message.channel.send("The server has no icon")
+} else {
+  
     const embed = new MessageEmbed()
       .setTitle(`${message.guild.name}'s icon`)
       .setTimestamp()
@@ -15,5 +18,14 @@ module.exports = {
       .setColor("BLUE");
 
     message.channel.send(embed);
+  
+  /* or message.channel.send(new MessageEmbed()
+      .setTitle(`${message.guild.name}'s icon`)
+      .setTimestamp()
+      .setImage(icon)
+      .setFooter(message.author.username)
+      .setColor("BLUE"))
+   */
+    }
   },
 };
