9a03638d4a52a6396dffb33434bc4d12e961d9f5
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-26 16:04:25 +0100

    :tada: update createrole.js

--- src/commands/admin/createrole.js
+++ src/commands/admin/createrole.js
@@ -1,44 +1,36 @@
-const Discord = require('discord.js')
-const {MessageEmbed} = require('discord.js')
+const { MessageEmbed } = require("discord.js");
+const { errorEmbed } = require("../../utils/functions");
+
 module.exports = {
-name: "addrole",
-description: "This command creates a role with the name of what you say",
-category: "admin", 
-  async execute(client,message,args){
-    const XD = new Discord.MessageEmbed()
-    .setDescription('You do not have permission to use this command!')
-    .setColor("RANDOM");
-    if (!message.member.hasPermission("MANAGE_ROLES")){
-      return message.channel.send(XD)
-    };
-    const rolename = args.slice(0).join(" ");
-      
-    const embed = new Discord.MessageEmbed()
-    .setTitle('Please Specify')
-    .setDescription("Please specify what you want to name the role!")
-    .setColor("RANDOM");
-      
-    const UwU = new Discord.MessageEmbed()
-.setDescription('I don\'t have permission to do that command!')
-.setColor("RANDOM");
-    if(message.guild.me.hasPermission('MANAGE_ROLES')){
-        
-    if (!rolename) {message.channel.send(embed)};
-        
-    if (rolename){
+  name: "createrole",
+  description: "This command creates a role with the name of what you say",
+  category: "admin",
+  async execute(bot, message, args) {
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+      return message.channel.send("You need `MANAGE_ROLES` permission.");
+    }
+    const roleName = args[0];
+
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+      return errorEmbed("manage roles (Manage Roles)", message);
+    }
+
+    if (!roleName) {
+      message.channel.send("Please specify a role name!");
+    }
+
     message.guild.roles.create({
       data: {
-      name: rolename,
-      color: "BLUE"
-      }
-    })
-    const embed = new Discord.MessageEmbed()
-    .setTitle(`Created Role: ${rolename}`)
-    .setDescription(`Successfully created the \`${rolename}\` role`)
-    .setColor("RANDOM")
-    .setTimestamp();
-    message.channel.send(embed)
-    };
-    } else return message.channel.send(UwU)
-    }
-}
+        name: roleName,
+        color: "BLUE",
+      },
+    });
+
+    const embed = new MessageEmbed()
+      .setTitle(`Created Role: ${roleName}`)
+      .setDescription(`Successfully created the \`${roleName}\` role`)
+      .setColor("BLUE")
+      .setTimestamp();
+    message.channel.send(embed);
+  },
+};
