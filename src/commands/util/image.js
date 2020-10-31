deef8628268349f95ea8498614f2f01fc6b3fc65
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-31 12:37:33 +0100

    :hammer: format code

--- src/commands/util/image.js
+++ src/commands/util/image.js
@@ -1,51 +1,49 @@
 const cheerio = require("cheerio");
 const request = require("request");
 const { MessageEmbed } = require("discord.js");
+
 module.exports = {
-name: "image",
-aliases: ["randomimage","imagesearch"],
-description: "Search any image you want from google",
-category: "util",
-async execute(bot, message, args) {
-const text = args.join(" ");
-if (!text) return message.reply("What do you want me to search?");
-var parts = message.content.split(" ");
-var search = parts.slice(1).join(" ");
-var options = {
-url: "http://results.dogpile.com/serp?qc=images&q=" + search,
-method: "GET",
-headers: {
-"Accept": "text/html",
-"User-Agent": "Chrome"
-}
+  name: "image",
+  aliases: ["randomimage", "imagesearch"],
+  description: "Search any image you want from google",
+  category: "util",
+  async execute(bot, message, args) {
+    const text = args.join(" ");
+    if (!text) return message.reply("What do you want me to search?");
+    const parts = message.content.split(" ");
+    const search = parts.slice(1).join(" ");
+    const options = {
+      url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+      method: "GET",
+      headers: {
+        Accept: "text/html",
+        "User-Agent": "Chrome",
+      },
+    };
+
+    request(options, function (error, response, responseBody) {
+      if (error) {
+        return;
+      }
+
+      const $ = cheerio.load(responseBody);
+
+      const links = $(".image a.link");
+
+      const urls = new Array(links.length)
+        .fill(0)
+        .map((v, i) => links.eq(i).attr("href"));
+
+      if (!urls.length) {
+        return message.channel.send("No images found");
+      }
+
+      const randomIndex = Math.floor(Math.random() * urls.length);
+      const image = urls[randomIndex];
+      const embed = new MessageEmbed()
+        .setColor("BLUE")
+        .setImage(image);
+      message.channel.send(embed);
+    });
+  },
 };
-request(options, function(error, response, responseBody) {
-if (error) {
-
-        return;
-    }
-
-
-    $ = cheerio.load(responseBody); 
-
-
-    var links = $(".image a.link");
-
-
-    var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-
-    if (!urls.length) {
-       
-        return;
-    }
-  let pages = [urls[0]]
-let page = 1;
-const embed = new MessageEmbed()
-.setColor('RANDOM')
-
-.setImage(pages[page-1])
-message.channel.send(embed)
-
-}
-)}
-}
