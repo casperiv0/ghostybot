10f13f07a37ccf14e083201da1eb12497352de2c
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-11-01 07:47:22 +0100

    :tada: improved wiki command

--- src/commands/util/wiki.js
+++ src/commands/util/wiki.js
@@ -1,18 +1,34 @@
 const wiki = require("wikijs").default();
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "wiki",
   aliases: ["wikipediasearch", "wikipedia"],
   category: "util",
   description: "",
   async execute(bot, message, args) {
-    if (!args[0]) return message.channel.send("You must enter something to search!")
-        let result;
-        const search = await wiki.search(args.join(' '));
-        result = await wiki.page(search.results[0]);
-            let description = await result.summary();  
-                const embed = new MessageEmbed()
-                    .setDescription(`${description.slice(0, 2048)}`)
-               message.channel.send('', embed)
+    if (!args[0]) {
+      return message.channel.send("You must enter something to search!");
     }
+
+    const search = await wiki.search(args.join(" "));
+
+    if (!search.results[0]) {
+      return message.channel.send("No results found");
+    }
+
+    const result = await wiki.page(search.results[0]);
+    const description = await result.summary();
+    const title = result.raw.title;
+    const url = result.raw.fullurl;
+
+    const embed = new MessageEmbed()
+      .setTitle(`${title} (read more)`)
+      .setURL(url)
+      .setDescription(
+        `${description.slice(0, 2045)}${description.length > 2048 && "..."}`
+      )
+      .setColor("BLUE")
+      .setFooter(message.author.username);
+    message.channel.send("", embed);
+  },
 };
