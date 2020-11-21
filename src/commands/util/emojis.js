61f8fd24d4f0ca2587621ef56ec44731418e18f4
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-11-21 14:52:10 +0100

    :bug: fix: fixed emojis command when too many emojis

--- src/commands/util/emojis.js
+++ src/commands/util/emojis.js
@@ -14,14 +14,24 @@ module.exports = {
       else nonAnimated.push(e.toString());
     });
 
+    const animatedV =
+      animated.join(" ").length > 1024
+        ? `${animated.join(" ").slice(1010)}...`
+        : animated.join(" ");
+
+    const nonAnimatedV =
+      nonAnimated.join(" ").length > 1024
+        ? `${nonAnimated.join(" ").slice(1010)}...`
+        : nonAnimated.join(" ");
+
     const embed = BaseEmbed(message)
       .addField(
         `${lang.UTIL.ANIMATED}:`,
-        animated.length === 0 ? lang.GLOBAL.NONE : animated.join(" ")
+        animated.length === 0 ? lang.GLOBAL.NONE : animatedV
       )
       .addField(
         `${lang.UTIL.NON_ANIMATED}:`,
-        nonAnimated.length === 0 ? lang.GLOBAL.NONE : nonAnimated.join(" ")
+        nonAnimated.length === 0 ? lang.GLOBAL.NONE : nonAnimatedV
       );
 
     message.channel.send(embed);
