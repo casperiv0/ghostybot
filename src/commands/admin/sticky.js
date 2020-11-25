59b41ada6a44ff62eb940f3249cc8592e74ce88d
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-11-25 11:46:12 +0100

    :bug: fix: added max limit to sticky message of 1800chars

--- src/commands/admin/sticky.js
+++ src/commands/admin/sticky.js
@@ -7,18 +7,24 @@ module.exports = {
   botPermissions: ["MANAGE_MESSAGES"],
   memberPermissions: ["MANAGE_MESSAGES"],
   async execute(bot, message, args) {
-    message.delete();
-
     const stickyMsg = args.join(" ");
 
-    if (stickyMsg === "") {
+    if (!stickyMsg[0]) {
       return message.reply("Please provide a message");
+    }
+
+    if (stickyMsg.length > 1800) {
+      return message.channel.send(
+        "Your sticky message can not be longer than 1800 characters!"
+      );
     }
 
     const msg = `__***:warning: Sticky Message, Read Before Typing! :warning:***__ \n\n ${stickyMsg}`;
 
+    message.delete();
+
     const stickyMessage = await message.channel.send(msg);
 
-    addSticky(stickyMessage.id, message.channel.id, msg);
+    await addSticky(stickyMessage.id, message.channel.id, msg);
   },
 };
