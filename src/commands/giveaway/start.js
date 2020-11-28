2a08ae82101428a4130729c40857f7a18a5d89e1
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-11-28 20:39:34 +0100

    :bug: fix: 'val is not a non-empty string'

--- src/commands/giveaway/start.js
+++ src/commands/giveaway/start.js
@@ -13,6 +13,18 @@ module.exports = {
     const winnerCount = args[1];
     const prize = args.slice(2).join(" ");
 
+    if (!time) {
+      return message.channel.send("Please provide an end time");
+    }
+
+    if (!winnerCount) {
+      return message.channel.send("Please provide a winner count");
+    }
+
+    if (!prize) {
+      return message.channel.send("Please provide a prize");
+    }
+
     bot.giveawayManager.start(message.channel, {
       time: ms(time),
       prize: prize,
