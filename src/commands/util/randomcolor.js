12b16b88aa8abb4492779d3278d4f231b12bf3f4
Oleg <73775612+Tegnio6882@users.noreply.github.com>
2020-12-19 17:26:23 +0300

    it is important to add semicolons

--- src/commands/util/randomcolor.js
+++ src/commands/util/randomcolor.js
@@ -7,7 +7,7 @@ module.exports = {
   aliases: ["color"],
   execute(bot, message) {
     const color = Math.floor(Math.random()*16777215).toString(16);
-    const preview = `https://api.no-api-key.com/api/v2/color?hex=${color}`
+    const preview = `https://api.no-api-key.com/api/v2/color?hex=${color}`;
 
     const embed = BaseEmbed(message)
       .setThumbnail(preview)
