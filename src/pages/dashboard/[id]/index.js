e2306515d5cd867770f5d87539dbab9e2868cae3
Bicepsfako <34413473+Bicepsfako@users.noreply.github.com>
2020-12-27 01:05:02 +0300

    Replaced 6

--- src/pages/dashboard/[id]/index.js
+++ src/pages/dashboard/[id]/index.js
@@ -20,7 +20,7 @@ const Guild = ({ guild, isAuth }) => {
   return (
     <>
       <Head>
-        <title>Viewing {guild?.name} / GhostyBot Dashboard</title>
+        <title>Viewing {guild?.name} / {dashboard.botName} Dashboard</title>
       </Head>
       <div className="page-title">
         <h4>Current guild: {guild.name}</h4>
