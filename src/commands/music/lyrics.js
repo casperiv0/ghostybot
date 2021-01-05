d48b8de35fc0788697c922a0a20991a0555295ff
Xaliks <64866197+Xaliks@users.noreply.github.com>
2021-01-05 20:29:13 +0300

    Lyrics

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -1,36 +1,30 @@
-const lyricsFinder = require("lyrics-finder");
+const fetch = require("node-fetch");
 const BaseEmbed = require("../../modules/BaseEmbed");
 const Logger = require("../../modules/Logger");
 
 module.exports = {
   name: "lyrics",
   aliases: ["ly"],
-  description: "Get lyrics for the currently playing song",
+  description: "Get lyrics for the song",
   category: "music",
-  async execute(bot, message) {
-    const lang = await bot.getGuildLang(message.guild.id);
-    const queue = await bot.player.getQueue(message);
-    const song = bot.player.nowPlaying(message);
-    
-    if (!queue) {
-        return message.channel.send(lang.MUSIC.NO_QUEUE);
-      }
-
-    let lyrics = null;
-
-    try {
-      lyrics = await lyricsFinder(song.title, "");
-      if (!lyrics) lyrics = `No lyrics found for ${song.title}.`;
-    } catch (error) {
-      lyrics = `No lyrics found for ${song.title}.`;
-    }
+  requiredArgs: ["song title"],
+  async execute(bot, message, args) {
+    const title = encodeURIComponent(args.join(" "));
+    const song = await (
+      await fetch("https://some-random-api.ml/lyrics?title=" + title)
+    ).json();
+    const songTitle = song.title;
+    const songAuthor = song.author;
+    const songThumbnail = song.thumbnail.genius;
+    const songLyrics = song.lyrics;
 
     let lyricsEmbed = BaseEmbed(message)
-      .setTitle(`${song.title}`)
-      .setDescription(lyrics);
+      .setAuthor(songAuthor)
+      .setTitle(songTitle)
+      .setDescription(songLyrics)
+      .setThumbnail(songThumbnail);
 
-    if (lyricsEmbed.description.length >= 2048)
-      lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
+    if (lyricsEmbed.description.length >= 2048) lyricsEmbed.description = `${lyricsEmbed.description.substr(0, 2045)}...`;
     return message.channel.send(lyricsEmbed).catch(e => Logger.error("Lyrics", e));
-  }
+  },
 };
