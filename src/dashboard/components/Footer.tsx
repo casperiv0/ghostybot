17a01d75fb8328443a894114aefce6ca73c68e7e
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-05-26 14:31:36 +0200

    :bug: fix: incorrect select state

--- src/dashboard/components/Footer.tsx
+++ src/dashboard/components/Footer.tsx
@@ -1,9 +1,15 @@
 import { useRouter } from "next/router";
 import { useTranslation } from "react-i18next";
+import * as React from "react";
 
 const Footer: React.FC = () => {
+  const [locale, setLocale] = React.useState("");
   const router = useRouter();
   const { t } = useTranslation("footer");
+
+  React.useEffect(() => {
+    setLocale(window.localStorage.getItem("bot_locale") || "en");
+  }, []);
 
   function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
     const locale = e.target.value;
@@ -20,7 +26,7 @@ const Footer: React.FC = () => {
         {t("not_affiliated")}
       </p>
 
-      <select onChange={handleChange} className="select-language">
+      <select value={locale} onChange={handleChange} className="select-language">
         <option value="en">English</option>
         <option value="ru">Russian</option>
       </select>
