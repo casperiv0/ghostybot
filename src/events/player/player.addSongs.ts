358610a9e6588c3ae493272765ae2a993b4feef4
Totto16 <32566573+Totto16@users.noreply.github.com>
2021-07-07 18:08:07 +0200

    merge conflict fix

---
+++ src/events/player/player.addSongs.ts
@@ -0,0 +1,35 @@
+import { Queue, Song } from "distube";
+import Bot from "structures/Bot";
+import Event from "structures/Event";
+
+export default class PlayerAddSongEvent extends Event {
+  constructor(bot: Bot) {
+    super(bot, "addSong");
+  }
+
+  async execute(bot: Bot, queue: Queue, song: Song) {
+    try {
+      console.log("here");
+      console.log(queue);
+      console.log(song);
+
+      const channel = queue.textChannel;
+
+      if (!channel?.guild?.available) return;
+      const lang = await bot.utils.getGuildLang(channel?.guild?.id);
+
+      const embed = bot.utils
+        .baseEmbed({ author: song.user ?? null })
+        .setTitle(lang.MUSIC.ADDED_TO_QUEUE.replace("{song}", song.name as string))
+        .setAuthor(`${lang.MUSIC.REQUESTED_BY.replace("{user}", track.requestedBy.username)}`);
+
+      if (song.thumbnail) {
+        embed.setImage(song.thumbnail);
+      }
+
+      return channel?.send({ embeds: [embed] });
+    } catch (err) {
+      bot.utils.sendErrorLog(err, "error");
+    }
+  }
+}
