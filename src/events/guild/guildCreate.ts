14a7b4fd286e1ba2d08552a594fe3069f6835b8c
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-09-08 18:31:30 +0200

    :hammer: chore: add bot join message

--- src/events/guild/guildCreate.ts
+++ src/events/guild/guildCreate.ts
@@ -1,4 +1,4 @@
-import { Guild } from "discord.js";
+import * as DJS from "discord.js";
 import { Bot } from "structures/Bot";
 import { Event } from "structures/Event";
 
@@ -7,8 +7,34 @@ export default class GuildCreateEvent extends Event {
     super(bot, "guildCreate");
   }
 
-  async execute(bot: Bot, guild: Guild) {
+  async execute(bot: Bot, guild: DJS.Guild) {
     if (!guild) return;
     await bot.utils.addGuild(guild.id);
+
+    if (guild.systemChannel) {
+      const BUTTONS = [
+        new DJS.MessageButton()
+          .setLabel("Open Dashboard")
+          .setStyle("LINK")
+          .setURL("https://ghostybot.caspertheghost.me"),
+        new DJS.MessageButton()
+          .setLabel("Support Server")
+          .setStyle("LINK")
+          .setURL("https://discord.gg/XxHrtkA"),
+      ];
+
+      const embed = bot.utils
+        .baseEmbed({ author: bot.user! })
+        .setTitle("Hello!")
+        .setDescription(
+          `
+Thanks for inviting me to this Discord server!
+I am GhostyBot a special bot that can do a lot such as moderation, play games and music, etc!`,
+        );
+
+      const row = new DJS.MessageActionRow().addComponents(BUTTONS);
+
+      await guild.systemChannel.send({ embeds: [embed], components: [row] });
+    }
   }
 }
