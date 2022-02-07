9273f3e9566ea3d9882059bb6beed348043ecd62
Diabla <95551108+ddiablou@users.noreply.github.com>
2022-02-07 17:39:47 +0100

    chore: remove redundant logic and replace with `Guild#nameAcronym` (#349)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -49,12 +49,7 @@ const Dashboard: React.FC<Props> = ({ isAuth, guilds }: Props) => {
 
       <div className="grid">
         {guilds.map((guild) => {
-          // take the first letter of all the letters
-          const [firstLetter] = guild.name.split("");
-
-          // take the second word, then the first character of that word
-          const secondLetter = guild.name.split(" ")[1]?.split("")[0];
-
+          
           return (
             <Link key={guild.id} href={guild.inGuild ? `/dashboard/${guild.id}` : "#"}>
               <a
@@ -65,8 +60,7 @@ const Dashboard: React.FC<Props> = ({ isAuth, guilds }: Props) => {
               >
                 {guild.icon === null ? (
                   <div className="guild-card-img no-image">
-                    {firstLetter}
-                    {secondLetter}
+                    {guild.nameAcronym}
                   </div>
                 ) : (
                   <Image
